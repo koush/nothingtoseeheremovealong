3541be7e11b16fbd4831fa5a1aac0bb4b648f7bb
Koushik Dutta <koushd@gmail.com>
2011-11-22 21:12:52 -0800

    initial

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/sbin/sh
+# don't use nfc or tag if the device doesn't support it.
+# check for support to see if the base rom has those apks.
+if [ ! -f "/system/app/NfcGoogle.apk" -a ! -f "/system/app/Nfc.apk" ]
+then
+    rm -f /tmp/gapps/system/app/NfcGoogle.apk
+fi
+if [ ! -f "/system/app/TagGoogle.apk" -a ! -f "/system/app/Tag.apk" ]
+then
+    rm -f /tmp/gapps/system/app/TagGoogle.apk
+fi
+
+for f in $(ls /tmp/gapps/system/app | sed s/Google//g)
+do
+    rm /system/app/$f
+done
+
+cp -R /tmp/gapps/system/* /system
